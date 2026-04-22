70181da42681539a4ea1f79e7c63c3bf0509f8c1
Tom <2106163819@qq.com>
2026-04-23 01:44:30 +0800

    add c project

---
+++ C++ C/if语句语法规范学习与应用.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+
+int main()
+{
+  /*int a = 0;
+    int b = 2;
+    if (a == 1)
+        if (b == 2)
+            printf("hehe\n");
+        else
+            printf("haha\n");*/
+
+    /*int a = 0;
+    int b = 2;
+    if (a == 1)
+        if (b == 2)
+            printf("hehe\n");
+    else
+        printf("haha\n");*/
+
+    /*int a = 0;
+    int b = 2;
+    if (a == 1)
+    {
+        if (b == 2)
+            printf("hehe\n");
+    }
+        else
+            printf("haha\n");
+
+    int a = 0;
+    int b = 2;
+    if (a == 1)
+        {
+            if (b == 2)
+            {
+                printf("hehe\n");
+            }
+        }
+        else
+        {
+            printf("haha\n");
+        }*/
+    
+    //if书写形式的对比:
+    /*if (condition)
+    {
+        return x;
+    }
+    return y;
+
+    if (condition)
+    {
+        return x;
+    }
+    else
+    {
+        return y;
+    }*/
+
+    /*int num = 1;
+    if (num == 5)
+    {
+        printf("hehe\n");
+    }
+
+    int num = 1;
+    if (5 == num)
+    {
+        printf("hehe\n");
+    }*/
+
+    //判断一个数是否为奇数
+    /*int num = 0;
+    scanf("%d,&num")
+        if ("num % 2 == 1");//一个数除以2余1是奇数，反之则是偶数
+            printf("奇数\n")；
+        else
+            printf("偶数\n");*/
+
+    /*int i = 1;
+    while (i < 100)
+    {
+        printf("%d", i);
+        i++*/
+    }
+
+    int i = 1;
+    while (i < 100)
+    {
+        printf("%d", i);
+        i += 2;
+    }
+
+    return 0;
+}
