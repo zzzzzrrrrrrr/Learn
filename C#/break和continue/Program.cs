de538b302bcf80f591688b7594d21fae4fa8f10f
Tom <2106163819@qq.com>
2026-05-15 15:31:23 +0800

    C#

---
+++ C#/break和continue/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _001_break使用方法
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 100;i++)
+            {
+                if(i % 2 == 0)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
+namespace _002_continue使用方法
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
