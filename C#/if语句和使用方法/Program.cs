87f4b0b05b263259729d196f2e31984e55425865
Tom <2106163819@qq.com>
2026-06-01 15:20:12 +0800

    C#

---
+++ C#/if语句和使用方法/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+/*namespace _001_if语句
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if(age <= 16)
+            {
+                Console.WriteLine("可以进入");
+            }
+            else
+            {
+                Console.WriteLine("年龄太大不能进入");
+            }
+
+            Console.WriteLine("程序结束");
+        }
+
+    }
+}*/
+
+namespace _002_if嵌套
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age >= 18 && age <= 30)
+            {
+                Console.WriteLine("可以参加活动");
+
+                if(age % 2 == 1)
+                {   
+                    Console.WriteLine("获得奖品");
+                }
+                else
+                {
+                    Console.WriteLine("没有获得奖品");
+                }
+            }
+            else
+            {
+                Console.WriteLine("不能参加活动");
+            }
+            Console.WriteLine("程序结束");
+        }
+    }
+}
+
+namespace _003_if
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
