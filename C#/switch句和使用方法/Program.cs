786b89ec1afc8fc1ee2e6055c005db1476e8c868
Tom <2106163819@qq.com>
2026-06-07 16:53:44 +0800

    C#

---
+++ C#/switch句和使用方法/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace _001_switch语句
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            switch(num)
+            {
+                case 1:
+                    Console.WriteLine("矿泉水");
+                    break;
+                case 2:
+                    Console.WriteLine("红茶");
+                    break;
+                case 3:
+                    Console.WriteLine("绿茶");
+                    break;
+                case 4:
+                    Console.WriteLine("雪碧");
+                    break;
+                case 5:
+                    Console.WriteLine("可口可乐");
+                    break;
+                case 6:
+                    Console.WriteLine("脉动");
+                    break;
+                default:
+                    Console.WriteLine("没有你要购买的商品");
+                    break;
+
+            }
+
+        }
+
+    }
+}
+
+namespace _002_switch语句
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            int weeknum = Convert.ToInt32(Console.ReadLine());
+            switch (weeknum)
+            {
+                case 1:
+                    Console.WriteLine("Arduino");
+                    break;
+                case 2:
+                    Console.WriteLine("Arduino");
+                    break;
+                case 3:
+                    Console.WriteLine("C++");
+                    break;
+                case 4:
+                    Console.WriteLine("C++");
+                    break;
+                case 5:
+                    Console.WriteLine("MySQL");
+                    break;
+                case 6:
+                    Console.WriteLine("scratch");
+                    break;
+                default:
+                    Console.WriteLine("Linux");
+                    break;
+            }
+        }
+    }
+}
+
+namespace _003_switch语句
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
