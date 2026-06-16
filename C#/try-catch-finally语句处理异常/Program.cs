38737112781fd54d657c7e52490ae4d4ac8d5c49
Tom <2106163819@qq.com>
2026-06-16 13:58:07 +0800

    C#

---
+++ C#/try-catch-finally语句处理异常/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _001_caycatchfinally处理异常
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] myarr = { 1, 2, 3, 4 };
+
+            try
+            {
+                int ret = myarr[4];
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("数组下标越界了0.0");
+            }
+            finally
+            {
+                Console.WriteLine("请输入正确的数组下标");
+            }
+        }
+    }
+}
