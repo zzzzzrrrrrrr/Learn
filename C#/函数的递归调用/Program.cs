cb0f960caedcd9587cab4697e2feadfda17e3967
Tom <2106163819@qq.com>
2026-06-22 00:23:00 +0800

    C#

---
+++ C#/函数的递归调用/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _001_函数的递归调用
+{
+    class Test
+    {
+        static int F(int n)
+        {
+            if(n ==1)
+            {
+                return 1;
+            }
+            int result = n * F(n - 1);
+            return result;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine(F(10));
+        }
+    }
+}
