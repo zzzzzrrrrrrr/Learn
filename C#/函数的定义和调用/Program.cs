a00dc85f3bc96d47b70d70ca3f3e676767283f00
Tom <2106163819@qq.com>
2026-06-24 12:48:37 +0800

    C#

---
+++ C#/函数的定义和调用/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _001_函数
+{
+    class Program
+    {
+        static void Testadvice()
+        {
+            Console.WriteLine("Michael Jackson");
+        }
+
+        static bool TrueUsername(string username)
+        {
+            Console.WriteLine("检验逻辑修改");
+            return true;
+        }
+
+        static void Username(string username)
+        {
+            Console.WriteLine(username);
+        }
+
+        static void Add1()
+        {
+            int result = 3 + 4;
+        }
+
+        static int Add2(int a,int b)
+        {
+            int result = a + b;
+            return result;
+        }
+   
+        static void Main(string[] args)
+        {
+            Testadvice();
+            Testadvice();
+
+            string s1 = "Michael Jackson";
+            Username(s1);
+
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Add1();
+            Add2(a, b);
+        }
+    }
+}
