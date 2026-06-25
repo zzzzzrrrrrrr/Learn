8856130b699a23df87e17dc62ece8e0079bc026c
Tom <2106163819@qq.com>
2026-06-25 12:31:50 +0800

    C#

---
+++ C#/函数重载/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _001_函数重载
+{
+    class Test
+    {
+        static int MaxValue(int[] array)
+        {
+            int max = array[0];
+            for (int i = 1; i < array.Length; i++) 
+            {
+                max = array[i];
+            }
+            return max;
+        }
+
+        static double MaxValue(double[] array)
+        {
+            double max = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                max = array[i];
+            }
+            return max;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxValue(new int[] { 1, 28, 33, 40, 51, 6 });
+
+        }
+    }
+}
