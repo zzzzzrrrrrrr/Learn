9499f6bd2f675a8d57aef29f00b8e078a0b79eba
Tom <2106163819@qq.com>
2026-06-27 19:28:38 +0800

    C#

---
+++ C#/枚举类型/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _001_枚举类型
+{
+    class Test
+    {
+        enum RoleType //枚举类型的名字enum
+        {
+            Mage,Archer,Assassin,Tank,Support,Warrior
+        }
+
+        static void Main(string[] args)
+        {
+            //1.法师(Mage),2.射手(Archer),3.刺客(Assassin),4.坦克(Tank),5.辅助(Support),6.战士(Warrior)
+
+            RoleType roleType = RoleType.Archer;
+            
+            if (roleType == RoleType.Assassin)
+            {
+
+            }
+
+        }
+    }
+}
+
+namespace _002_枚举类型的练习
+{
+    class Program
+    {
+        enum GameState
+        {
+            //菜单，游戏中，暂停，失败，成功
+            Menu,Running,Pauese,Fail,Success
+        }
+        enum Week
+        {
+            //一周7天
+            Sun,Mon,Tue,Wed,Thu,Fri,Sat
+        }
+
+        static void Main(string[] args)
+        {
+            GameState gamestate = GameState.Menu;
+            gamestate = GameState.Running;
+
+            if (gamestate == GameState.Menu)
+            {
+
+            }
+            Console.WriteLine(gamestate);
+
+            Week day = Week.Mon;
+            day = Week.Tue;
+            Console.WriteLine(day);
+        }
+    }
+}
