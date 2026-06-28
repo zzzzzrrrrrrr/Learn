97a574c0b0d79ed596c87f2c6a37686cbce908f6
Tom <2106163819@qq.com>
2026-06-28 08:15:36 +0800

    C#

---
+++ C#/属性/Customer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 属性_
+{
+    class Customer
+    {
+        private string name;
+        private string address;
+        private int age;
+        private string createTime;
+
+        //public void SetAge(int age)
+        //{ 
+        //    this.age = age;
+        //}
+        //public int GetAge()
+        //{
+        //    return age;
+        //}
+        //public string Name
+        //{
+        //    get
+        //    {
+        //        return name;
+        //    }
+        //    set
+        //    {
+        //        name = value;
+        //    }
+        //}
+
+        //public int Age
+        //{
+        //    get
+        //    {
+        //        return age;
+        //    }
+        //    set
+        //    {
+        //        if (value < 0) return;
+        //        age = value;
+        //    }
+        //}
+
+        public string Name
+        {
+            get;set;
+        }
+        public int Age { get; set; }
+
+        //public Customer(string name,string address,int age ,string createTime)
+        //{
+        //    this.name = name;
+        //    this.address = address;
+        //    this.age = age;
+        //    this.createTime = createTime;
+        //}
+
+        public void Show()
+        {
+            Console.WriteLine("名字 " + name);
+            Console.WriteLine("地址" + address);
+            Console.WriteLine("年龄" + age);
+            Console.WriteLine("创建时间" + createTime);
+        }
+    }
+}
---
+++ C#/属性/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using 属性_;
+
+namespace _001_属性
+{
+    class Program
+    {
+    static void Main(string[] args)
+        {
+            Customer lisi = new Customer();
+
+            lisi.Age = 23;
+
+            Console.WriteLine(lisi.Age);
+
+            lisi.Name = "edu";
+            Console.WriteLine(lisi.Name);
+
+            int age = 12;
+
+            //匿名类型
+            var age2 = 45;
+
+            var name = "edu";
+
+        }
+    }
+}
