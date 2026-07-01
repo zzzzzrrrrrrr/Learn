bf4201f72e2d1d12103edd550a426552c89cfef7
Tom <2106163819@qq.com>
2026-07-01 14:14:16 +0800

    C#

---
+++ C#/委托/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _001_委托
+{
+    class Program
+    {
+        static double Multiply(double param1, double param2)
+        {
+            return param1 + param2;
+        }
+        static double Divide(double param1, double param2)
+        {
+            return param1 / param2;
+        }
+
+        static void Test()
+        {
+            Console.WriteLine("Test");
+        }
+
+        delegate double MyDelegate(double param1, double param2);
+        delegate void MyDelegate2();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Multiply(2.3, 2));
+            Console.WriteLine(Divide(4.5, 3));
+
+            MyDelegate Delegate1 = Multiply;
+            Console.WriteLine(Delegate1(2,4));
+
+            Delegate1 = Divide;
+            Console.WriteLine(Delegate1(2, 4));
+
+            MyDelegate2 Delegate2 = Test;
+            Console.WriteLine(Delegate2);
+
+        }
+    }
+}
