05b8f2fa699bd156ca0b7e7e469d722df04da221
Tom <2106163819@qq.com>
2026-07-02 12:52:56 +0800

    C#

---
+++ C#/委托案例/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _001_委托案例
+{
+    class Program
+    {
+        delegate void OnDieDelegate();
+        static void Play(OnDieDelegate onDie)
+        {
+            Console.WriteLine("做任务");
+            Console.WriteLine("玩家正在战斗");
+            Console.WriteLine("死亡");
+
+            if (onDie != null)
+            {
+                onDie();
+            }
+        }
+
+        static void ShowDieUI()
+        {
+            Console.WriteLine("显示游戏死亡后的UI");
+            Console.WriteLine("返回首页UI");
+
+        }
+
+        static void Main(string[] args)
+        {
+            //A主角
+            //B 游戏UI 菜单
+            Console.WriteLine("");
+            Play(ShowDieUI);
+        }
+    }
+}
