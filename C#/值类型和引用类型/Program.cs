ec304ae9242107e03a776e1b60037bdad90a6186
Tom <2106163819@qq.com>
2026-07-07 13:14:22 +0800

    C#

---
+++ C#/值类型和引用类型/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _001_值类型和引用类型
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+            float b = 3.45f;
+            bool c = true;
+
+            string name = "fuck";
+
+            int[] array1 = new int[] { 1, 2, 3 };
+            string[] array2 = new string[] {"oh","yeah","fucking","coming"};
+
+
+        }
+    }
+
+}
