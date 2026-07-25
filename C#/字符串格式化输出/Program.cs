a17abd9cceb7cce333bdcc13a7718bac81ea45de
Tom <2106163819@qq.com>
2026-07-25 13:58:00 +0800

    C#

---
+++ C#/字符串格式化输出/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _002_字符串输出
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            int a = 23, b = 45;
+
+            Console.WriteLine("{0}+{1}={2}",a,b,a + b);
+
+            Console.WriteLine("两个数字相加{0}+{0}={2}", 34, 123, 4);
+
+            Console.WriteLine("两个数字相加{0}+{0}={3}", 34, 123, 4);
+
+
+        }
+    }
+}
